b95cb2659f1035f9934a63cdb0649ce47ddb738d
ElizabethCowell <80732868+ElizabethCowell@users.noreply.github.com>
2021-04-12 14:42:16 -0400

    Class Car

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Chevy";
+            myCar.Model = "Equinox";
+            myCar.Year = 2013;
+
+            Console.WriteLine(myCar.Model);
+
+        }
+    }
+}
